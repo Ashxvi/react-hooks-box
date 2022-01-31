f5a0a0c37eac57ccd12f67306316217d21e6941d
Achraf Mahha <achraf.mahha-extern@renault.com>
2022-02-01 00:49:55 +0100

    feat: add useAxios hook - first release

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 
 npm run build
 cp package.json ./dist
+cp README.md ./dist
 # or, if you need to have package.json "main" entry different,
 # e.g. for being able to use `npm link`, you need to replace "main" value:
 # sed 's#"main": "./dist/index.js"#"main": "./index.js"#' package.json > ./dist/package.json
